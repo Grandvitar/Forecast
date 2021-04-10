d3f8b75056b1d5e4e378b60db0fa79efce2f7903
Malex1982 <malex1982@mail.ru>
2021-04-10 18:16:40 +0300

    'v.1'

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import weather
 import datetime
 
-json = weather.currentWeather()
-json_forecast = weather.weekForecast(json)
+current = weather.currentWeather()
+json_forecast = weather.weekForecast(current)
 
 date_now = datetime.datetime.today()
 
 def main():
     if not weather.namespace.date:
-        weather.showCurrentWeather(json)
-        weather.showWeekForecast(json, json_forecast)
+        weather.showCurrentWeather(current)
+        weather.showWeekForecast(current, json_forecast)
     elif (datetime.datetime.strptime(weather.namespace.date, '%d.%m.%Y') - date_now) > datetime.timedelta(7):
         print('***Прогноз можно запросить только на ближайшие 7 дней***')
     else:
-        weather.showDateForecast(json, json_forecast)
+        weather.showDateForecast(current, json_forecast)
 
 main()
